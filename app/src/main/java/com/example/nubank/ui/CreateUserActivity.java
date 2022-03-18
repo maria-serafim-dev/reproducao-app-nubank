a35a265192518a03ad64a60972b859025939f6b2
maria-serafim-dev <maria.serafim.dev@gmail.com>
2022-03-17 21:27:37 -0300

    Validation of edit fields and error messages

--- app/src/main/java/com/example/nubank/ui/CreateUserActivity.java
+++ app/src/main/java/com/example/nubank/ui/CreateUserActivity.java
@@ -6,6 +6,7 @@ import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
@@ -22,6 +23,7 @@ public class CreateUserActivity extends AppCompatActivity {
     private FirebaseAuth auth = FirebaseAuth.getInstance();
 
     ActivityCreateUserBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,32 +34,49 @@ public class CreateUserActivity extends AppCompatActivity {
     }
 
     private void clickListenerButtonEmailPassword() {
-        binding.button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        binding.button.setOnClickListener(view -> {
+            if (validarCampos()) {
                 String email = binding.editEmail.getText().toString();
                 String password = binding.editPassword.getText().toString();
 
                 auth.createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(CreateUserActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Log.i("signIn", "Sucesso ao logar usuário");
-                            Toast.makeText(getApplicationContext(),"Usuário Criado com sucesso",Toast.LENGTH_LONG).show();
-                            nextActivity();
-                        } else {
-                            Log.i("signIn", "Erro ao logar usuário");
-                            Log.i("signIn", task.getResult().toString());
-                            Toast.makeText(getApplicationContext(),"Usuário Criado com sucesso",Toast.LENGTH_LONG).show();
-                        }
-                    }
-                });
+                        .addOnCompleteListener(CreateUserActivity.this, task -> {
+                            if (task.isSuccessful()) {
+                                Log.i("signIn", "Sucesso ao logar usuário");
+                                Toast.makeText(getApplicationContext(), "Usuário Criado com sucesso", Toast.LENGTH_LONG).show();
+                                nextActivity();
+                            } else {
+                                Log.i("signIn", "Erro ao logar usuário");
+                                Log.i("signIn", task.getResult().toString());
+                                Toast.makeText(getApplicationContext(), "Usuário Criado com sucesso", Toast.LENGTH_LONG).show();
+                            }
+                        });
             }
         });
     }
 
-    public void nextActivity(){
+    private boolean validarCampos() {
+        if (TextUtils.isEmpty(binding.editEmail.getText())) {
+            binding.editEmail.setError("Dígite um email");
+            binding.editEmail.requestFocus();
+            return false;
+        } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(binding.editEmail.getText()).matches()) {
+            binding.editEmail.setError("E-mail inválido");
+            binding.editEmail.requestFocus();
+            return false;
+        }
+
+        if (TextUtils.isEmpty(binding.editPassword.getText())) {
+            binding.editPassword.setError("Dígite uma senha");
+            binding.editPassword.requestFocus();
+            return false;
+        }
+
+        return true;
+
+    }
+
+    public void nextActivity() {
         Intent intent = new Intent(this, Authetication.class);
         startActivity(intent);
     }
